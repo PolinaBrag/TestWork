d6f25c278cfd491781eaed5e07704b756f89f445
Полина Брагина <polyandriys14@mail.ru>
2021-12-28 14:37:11 +0300

    Add cycle for selecting numbers greater than 8

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -5,13 +5,13 @@
 Console.WriteLine("Enter count of array numbers:");
 int countOFArrayNumbers = int.Parse(Console.ReadLine());
 
-int[] firstArray = new int [countOFArrayNumbers];
+int[] firstArray = new int[countOFArrayNumbers];
 
 void FillArray(int[] array1)
 {
     for (int i = 0; i < array1.Length; i++)
     {
-        array1[i] = new Random().Next(- 50, 50);
+        array1[i] = new Random().Next(-50, 50);
     }
 }
 
@@ -23,7 +23,23 @@ void PrintArray(int[] array2)
     }
 }
 
-Console.Write("Your array: [ " );
+Console.Write("Your array: [ ");
 FillArray(firstArray);
 PrintArray(firstArray);
 Console.WriteLine("]");
+
+int checkNumber = 8;
+int[] newArray = new int[firstArray.Length];
+
+ Console.Write("Your changed array with numbers greater than 8: [ ");
+
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i] >= checkNumber)
+        {
+            newArray[i] = firstArray[i];
+            Console.Write(newArray[i] + " ");
+        }
+    }
+    Console.WriteLine("]");
+
